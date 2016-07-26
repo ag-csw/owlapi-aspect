6bd45c0c0941e4e84483727b6b29120f8d994712
lars <l.parmakerli@googlemail.com>
2016-07-26 16:36:34 +0200

    rename error to exception

--- src/main/java/de/fuberlin/csw/aspect_owlapi/util/NotAnAspectAnnotationException.java
+++ src/main/java/de/fuberlin/csw/aspect_owlapi/util/NotAnAspectAnnotationException.java
@@ -3,7 +3,7 @@ package de.fuberlin.csw.aspect_owlapi.util;
 /**
  * Created by lars on 02.05.16.
  */
-public class NotAnAspectAnnotationError extends RuntimeException{
+public class NotAnAspectAnnotationException extends RuntimeException{
 
 
 
